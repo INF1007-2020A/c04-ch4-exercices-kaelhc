adefc0e1b659883f3f84c800575ba0ac9bcc1002
kaelhc <karim.el-hosni@polymtl.ca>
2020-09-15 23:26:23 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -17,8 +17,7 @@ def remove_third_char(string: str) -> str:
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
     x=string.find(old_char)
-
-  return 
+    return 
 
 
 def get_number_of_char(string: str, char: str) -> int:
