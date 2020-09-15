8835b979b8379ff3bf8b48b069ad2d2ff152b699
kaelhc <karim.el-hosni@polymtl.ca>
2020-09-15 23:29:06 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -21,7 +21,8 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
-    for i in 
+    for i in string :
+        if string
     pass
 
 
