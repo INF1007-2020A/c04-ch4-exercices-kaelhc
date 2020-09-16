8f3318a61d91a9e0c6c6cc0dbb7a1f318d9c6756
kaelhc <71066868+kaelhc@users.noreply.github.com>
2020-09-16 09:29:47 +0300

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -21,9 +21,11 @@ def replace_char(string: str, old_char: str, new_char: str) -> str:
 
 
 def get_number_of_char(string: str, char: str) -> int:
+     c=0
     for i in string :
-        if string[i]==char
-    pass
+        if string[i]==char :
+               c+=1
+        return c 
 
 
 def get_number_of_words(sentence: str, word: str) -> int:
